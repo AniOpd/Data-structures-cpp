d3f7db3b7b3b916cee34630fccd8c786e73e5a3e
AniOpd <106003334+AniOpd@users.noreply.github.com>
2022-08-06 20:34:12 +0530

    intial codes

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Namste Dunia:)" << endl;
+    int a = 4, b = 9, sum = a + b;
+    cout << sum << endl;
+    char c = 'a';
+    cout << c << endl;
+    int size=sizeof(b);
+    cout<<size<<endl;
+    char ch=64;
+    cout<<ch<<endl;
+    unsigned int d=-56;
+    cout<<d<<endl;
+    cout<<b/a<<endl;
+}
